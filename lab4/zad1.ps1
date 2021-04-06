a19186770026fc62763ea72f7b8c022d5621cd6c
hdlugosz <79986639+hdlugosz@users.noreply.github.com>
2021-04-06 20:17:16 +0200

    Create zad1.ps1

---
+++ lab4/zad1.ps1
@@ -0,0 +1,26 @@
+﻿# Tablica partycji  – sposób zapisu informacji o partycjach na dysku twardym.
+
+diskpart
+
+#wylistowanie dysków (sprawdzenie tablicy partycji)
+list disk
+
+select disk 3  # w tym przypadku byl to pendrive
+
+#wylistowanie partycji
+list partiton
+
+select part 1
+
+#przypisanie litery
+assign letter = X
+
+#stworzenie partycji
+create partition primary size = 1000 # tworzy partycje o rozmiarze 1000mb
+
+#formatowanie partycji
+format fs=ntfs
+
+#zamontowanie dysku do katalogu
+
+assign mount=C:\Users\huber\Desktop\katalog
