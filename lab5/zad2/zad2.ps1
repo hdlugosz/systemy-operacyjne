aab8710091c7c366f3e9fe592e2b018549ba760e
hdlugosz <79986639+hdlugosz@users.noreply.github.com>
2021-04-19 18:01:14 +0200

    zad2

---
+++ lab5/zad2/zad2.ps1
@@ -0,0 +1,25 @@
+﻿# Get the path from which the script was run
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+
+# Change to the path where the script was run
+cd $dir
+
+# Create shortcut
+
+$Source = ".\shortcut.lnk"
+$Destination = "$dir.\origin_file.txt"
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($Source)
+$Shortcut.TargetPath = $Destination
+$Shortcut.Save()
+
+# Create SymbolicLink
+# Need to open Powershell as Administrator 
+New-Item -ItemType SymbolicLink -Path .\symLink.txt -Target .\origin_file.txt
+
+# Create HardLink
+New-Item -ItemType HardLink -Path .\hardLink.txt -Target .\origin_file.txt
+
+# Create Junction
+New-Item -ItemType  Junction -Path .\junction.txt -Target C:/Windows/System32/ 
